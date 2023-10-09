435b61ed8425d30b53336d42452460ec5c45e506
Biruk-ak <birukaklilu0110@gmail.com>
2023-10-09 21:41:52 +0300

    feat(shared): add kafka_client library suite for platform services

---
+++ libs/shared/include/kafka_client_6.h
@@ -0,0 +1,105 @@
+#ifndef SOC_SHARED_KAFKA_CLIENT_6_H
+#define SOC_SHARED_KAFKA_CLIENT_6_H
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define SOC_KAFKA_CLIENT_6_MAX 8192
+
+typedef struct {
+  uint64_t id; uint64_t tenant_id; uint32_t severity; uint32_t flags;
+  int64_t created_at; int64_t updated_at; double score; double confidence;
+  char name[256]; char description[2048]; char source[128];
+  char mitre[64]; uint8_t payload[4096]; size_t payload_len;
+} soc_kafka_client_6_rec_t;
+
+typedef struct { soc_kafka_client_6_rec_t *items; size_t capacity; size_t count; uint64_t seq; } soc_kafka_client_6_db_t;
+
+soc_kafka_client_6_db_t *soc_kafka_client_6_db_create(size_t n);
+void soc_kafka_client_6_db_destroy(soc_kafka_client_6_db_t *db);
+int soc_kafka_client_6_db_upsert(soc_kafka_client_6_db_t *db, const soc_kafka_client_6_rec_t *rec);
+int soc_kafka_client_6_db_remove(soc_kafka_client_6_db_t *db, uint64_t id);
+int soc_kafka_client_6_db_find(const soc_kafka_client_6_db_t *db, uint64_t id, soc_kafka_client_6_rec_t *out);
+size_t soc_kafka_client_6_db_scan(const soc_kafka_client_6_db_t *db, uint32_t min_sev, soc_kafka_client_6_rec_t *out, size_t max);
+uint64_t soc_kafka_client_6_hash(const void *p, size_t n);
+int soc_kafka_client_6_validate(const soc_kafka_client_6_rec_t *rec);
+int soc_kafka_client_6_to_json(const soc_kafka_client_6_rec_t *rec, char *buf, size_t len);
+void soc_kafka_client_6_metrics(const soc_kafka_client_6_db_t *db, char *buf, size_t len);
+int soc_kafka_client_6_correlate(soc_kafka_client_6_db_t *db, const soc_kafka_client_6_rec_t *seed, soc_kafka_client_6_rec_t *out, size_t max);
+int soc_kafka_client_6_fn_000(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_001(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_002(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_003(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_004(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_005(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_006(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_007(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_008(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_009(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_010(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_011(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_012(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_013(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_014(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_015(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_016(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_017(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_018(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_019(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_020(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_021(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_022(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_023(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_024(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_025(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_026(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_027(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_028(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_029(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_030(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_031(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_032(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_033(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_034(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_035(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_036(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_037(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_038(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_039(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_040(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_041(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_042(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_043(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_044(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_045(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_046(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_047(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_048(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_049(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_050(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_051(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_052(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_053(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_054(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_055(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_056(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_057(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_058(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_059(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_060(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_061(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_062(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_063(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_064(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_065(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_066(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_067(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_068(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_069(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_070(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_071(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_072(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_073(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_kafka_client_6_fn_074(soc_kafka_client_6_db_t *db, uint64_t a, const void *b, size_t blen);
+
+#endif /* SOC_SHARED_KAFKA_CLIENT_6_H */

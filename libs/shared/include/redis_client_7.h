88cab420a09edfae6013018b457ba8ab0e1d416c
Biruk-ak <birukaklilu0110@gmail.com>
2023-10-29 06:58:32 +0300

    feat(shared): add redis_client library suite for platform services

---
+++ libs/shared/include/redis_client_7.h
@@ -0,0 +1,105 @@
+#ifndef SOC_SHARED_REDIS_CLIENT_7_H
+#define SOC_SHARED_REDIS_CLIENT_7_H
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define SOC_REDIS_CLIENT_7_MAX 8192
+
+typedef struct {
+  uint64_t id; uint64_t tenant_id; uint32_t severity; uint32_t flags;
+  int64_t created_at; int64_t updated_at; double score; double confidence;
+  char name[256]; char description[2048]; char source[128];
+  char mitre[64]; uint8_t payload[4096]; size_t payload_len;
+} soc_redis_client_7_rec_t;
+
+typedef struct { soc_redis_client_7_rec_t *items; size_t capacity; size_t count; uint64_t seq; } soc_redis_client_7_db_t;
+
+soc_redis_client_7_db_t *soc_redis_client_7_db_create(size_t n);
+void soc_redis_client_7_db_destroy(soc_redis_client_7_db_t *db);
+int soc_redis_client_7_db_upsert(soc_redis_client_7_db_t *db, const soc_redis_client_7_rec_t *rec);
+int soc_redis_client_7_db_remove(soc_redis_client_7_db_t *db, uint64_t id);
+int soc_redis_client_7_db_find(const soc_redis_client_7_db_t *db, uint64_t id, soc_redis_client_7_rec_t *out);
+size_t soc_redis_client_7_db_scan(const soc_redis_client_7_db_t *db, uint32_t min_sev, soc_redis_client_7_rec_t *out, size_t max);
+uint64_t soc_redis_client_7_hash(const void *p, size_t n);
+int soc_redis_client_7_validate(const soc_redis_client_7_rec_t *rec);
+int soc_redis_client_7_to_json(const soc_redis_client_7_rec_t *rec, char *buf, size_t len);
+void soc_redis_client_7_metrics(const soc_redis_client_7_db_t *db, char *buf, size_t len);
+int soc_redis_client_7_correlate(soc_redis_client_7_db_t *db, const soc_redis_client_7_rec_t *seed, soc_redis_client_7_rec_t *out, size_t max);
+int soc_redis_client_7_fn_000(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_001(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_002(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_003(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_004(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_005(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_006(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_007(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_008(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_009(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_010(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_011(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_012(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_013(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_014(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_015(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_016(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_017(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_018(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_019(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_020(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_021(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_022(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_023(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_024(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_025(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_026(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_027(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_028(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_029(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_030(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_031(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_032(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_033(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_034(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_035(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_036(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_037(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_038(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_039(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_040(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_041(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_042(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_043(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_044(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_045(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_046(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_047(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_048(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_049(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_050(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_051(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_052(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_053(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_054(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_055(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_056(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_057(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_058(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_059(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_060(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_061(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_062(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_063(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_064(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_065(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_066(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_067(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_068(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_069(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_070(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_071(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_072(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_073(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+int soc_redis_client_7_fn_074(soc_redis_client_7_db_t *db, uint64_t a, const void *b, size_t blen);
+
+#endif /* SOC_SHARED_REDIS_CLIENT_7_H */
